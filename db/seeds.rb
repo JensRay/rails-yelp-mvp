87e8869109009002213bb11abe0eb2303cd4298f
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2020-11-11 15:59:33 +0100

    index and show running

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "deleting database..."
+Restaurant.destroy_all
+
+puts "creating new restaurants..."
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+5.times do
+  Restaurant.create(
+    name: Faker::JapaneseMedia::Conan.character,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: categories.sample
+    )
+
+end
+
+puts "done"
